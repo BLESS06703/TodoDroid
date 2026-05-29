37237fdd72c66b82c244fd063513a739833ad39a
BLESS06703 <blesskaunda056@gmail.com>
2026-05-30 01:46:40 +0200

    Wire up checkbox strikethrough and delete button

--- app/src/main/java/com/tododroid/TodoAdapter.java
+++ app/src/main/java/com/tododroid/TodoAdapter.java
@@ -3,6 +3,7 @@ package com.tododroid;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
@@ -17,9 +18,14 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
     
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView taskText;
+        public CheckBox checkbox;
+        public TextView btnDelete;
+        
         public ViewHolder(View itemView) {
             super(itemView);
             taskText = itemView.findViewById(R.id.task_text);
+            checkbox = itemView.findViewById(R.id.checkbox);
+            btnDelete = itemView.findViewById(R.id.btn_delete);
         }
     }
     
@@ -32,7 +38,32 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
     
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.taskText.setText(todoList.get(position));
+        String task = todoList.get(position);
+        holder.taskText.setText(task);
+        
+        holder.checkbox.setOnCheckedChangeListener(null);
+        holder.checkbox.setChecked(false);
+        holder.checkbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                holder.taskText.setPaintFlags(
+                    holder.taskText.getPaintFlags() | android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+                );
+                holder.taskText.setTextColor(0xFF666666);
+            } else {
+                holder.taskText.setPaintFlags(
+                    holder.taskText.getPaintFlags() & ~android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+                );
+                holder.taskText.setTextColor(0xFFE0E0E0);
+            }
+        });
+        
+        holder.btnDelete.setOnClickListener(v -> {
+            int pos = holder.getAdapterPosition();
+            if (pos != RecyclerView.NO_POSITION) {
+                todoList.remove(pos);
+                notifyItemRemoved(pos);
+            }
+        });
     }
     
     @Override
